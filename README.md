024dc203830cfbd05108e3527c87868886ddd3ad
Mathieu Lecarme <mlecarme@bearstech.com>
2021-04-22 21:50:10 +0200

    More flags, more doc.

--- README.md
+++ README.md
@@ -4,7 +4,26 @@ PHP proflier lite
 Something like [php-profiler](https://github.com/perftools/php-profiler)
 but opiniated, and usable inside a container with some ENV.
 
-* PHP >= 7.0
+* PHP >= 7.0 packaged by Debian
 * php-curl
-* php-tideways
-* target xhgui http upload
+* [php-tideways](https://github.com/tideways/php-xhprof-extension)
+* target [xhgui](https://github.com/perftools/xhgui) http upload
+
+Settings
+--------
+
+`PHP_PROFILER_TOKEN` : some random stuff
+
+`PHP_PROFILER_FLAGS` : comma separated flags :
+
+* CPU
+* MEMORY
+* NO_BUILTINS
+* NO_SPANS
+
+Demo
+----
+
+    make docker-image
+    make up
+    curl -H "php-profiler-token: toto"  -v http://localhost:8000/
--- profiler_lite.php
+++ profiler_lite.php
@@ -1,13 +1,24 @@
 <?php
 
 function _phpprofiler_init() {
-    //phpinfo();
-    //var_dump($_SERVER);
     $token = getenv('PHP_PROFILER_TOKEN');
     if ($token) {
         if ($token == $_SERVER['HTTP_PHP_PROFILER_TOKEN']) {
             register_shutdown_function('_phpprofiler_close');
-            tideways_enable();
+            $flags = 0;
+            $f = getenv('PHP_PROFILER_FLAGS');
+            if ($f) {
+                $names = array(
+                    'CPU' => TIDEWAYS_FLAGS_CPU,
+                    'MEMORY' => TIDEWAYS_FLAGS_MEMORY,
+                    'NO_BUILTINS' => TIDEWAYS_FLAGS_NO_BUILTINS,
+                    'NO_SPANS' => TIDEWAYS_FLAGS_NO_SPANS,
+                );
+                foreach (explode(',', $f) as $flag) {
+                    $flags |= $names[$flag];
+                }
+            }
+            tideways_enable($flags);
         }
     }
 }
